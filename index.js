4f39013c02e77b140c9a6f7005eca96a102e1ee9
akshara-45-ux <akshara707@gmail.com>
2026-02-26 13:32:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (text) => {
+    const messageData = {
+      text,
+      senderId: socket.id,
+      time: new Date().toLocaleTimeString(),
+    };
+
+    io.emit("message", messageData);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+
